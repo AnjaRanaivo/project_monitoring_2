97533bb06e1708bf3ce04736eea32e1afeaca2ea
OlyviaHasina <olyviahasina.razakamanantsoa@gmail.com>
2023-03-22 23:07:00 +0100

    add modal project 22/03/2023 23:06

--- lib/pm_login_web/live/project/my_projects_clients_2_live.ex
+++ lib/pm_login_web/live/project/my_projects_clients_2_live.ex
@@ -9,6 +9,7 @@ defmodule PmLoginWeb.Project.MyProjectsClients2Live do
   alias PmLogin.Monitoring.{Task, Project}
   alias PmLoginWeb.LiveComponent.{ClientModalRequestLive, DetailModalRequestLive, ProjectModalLive}
   alias PmLogin.Email
+  alias PmLogin.Services.ClientsRequest
 
   def mount(_params, %{"curr_user_id" => curr_user_id}, socket) do
     Services.subscribe()
@@ -27,6 +28,8 @@ defmodule PmLoginWeb.Project.MyProjectsClients2Live do
     {:ok,
       socket
       |> assign(
+        changeset: Services.change_clients_request(%ClientsRequest{}),
+        display_form: false,
         projects: Monitoring.list_projects(),
         curr_user_id: curr_user_id,
         show_project_modal: false,
@@ -75,6 +78,88 @@ defmodule PmLoginWeb.Project.MyProjectsClients2Live do
 
   def render(assigns) do
     ProjectView.render("active_client_2_index.html", assigns)
+  end
+
+  def handle_event("form-on", _params, socket) do
+    {:noreply,
+      socket
+      |> clear_flash()
+      |> assign(display_form: true)}
+  end
+
+  def handle_event("form-off", _params, socket) do
+    {:noreply, socket |> assign(display_form: false)}
+  end
+
+  def handle_event("change-request", params, socket) do
+    # IO.inspect(params)
+    {:noreply, socket}
+  end
+
+  def handle_event("send-request", %{"clients_request" => params}, socket) do
+    # Added uuid to the current params
+    params = Map.put_new(params, "uuid", Uuid.generate)
+
+    {entries, []} = uploaded_entries(socket, :file)
+    # IO.inspect(entries)
+
+    # urls = for entry <- entries do
+    #   Routes.static_path(socket, "/uploads/#{entry.uuid}#{Path.extname(entry.client_name)}")
+    # end
+
+    # IO.inspect urls
+
+    # consume_uploaded_entries(socket, :file, fn meta, entry ->
+    #   dest = Path.join("priv/static/uploads", "#{entry.uuid}#{Path.extname(entry.client_name)}")
+    #   File.cp!(meta.path, dest)
+    #  end)
+    # IO.inspect socket.assigns.uploads[:file].entries
+
+    case Services.create_clients_request(params) do
+      {:ok, result} ->
+        consume_uploaded_entries(socket, :file, fn meta, entry ->
+          ext = Path.extname(entry.client_name)
+          file_name = Path.basename(entry.client_name, ext)
+          dest = Path.join("priv/static/uploads", "#{file_name}#{entry.uuid}#{ext}")
+          File.cp!(meta.path, dest)
+        end)
+
+        {entries, []} = uploaded_entries(socket, :file)
+
+        urls =
+          for entry <- entries do
+            ext = Path.extname(entry.client_name)
+            file_name = Path.basename(entry.client_name, ext)
+            Routes.static_path(socket, "/uploads/#{file_name}#{entry.uuid}#{ext}")
+          end
+
+        Services.update_request_files(result, %{"file_urls" => urls})
+
+        {:ok, result} |> Services.broadcast_request()
+        # sending notifs to admins
+        curr_user_id = socket.assigns.curr_user_id
+        the_client = Services.get_active_client_from_userid!(curr_user_id)
+
+        Services.send_notifs_to_admins(
+          curr_user_id,
+          "Le client #{the_client.user.username} de la société #{the_client.company.name} a envoyé une requête intitulée \"#{result.title}\".",
+          5
+        )
+
+        Monitoring.broadcast_clients_requests({:ok, :clients_requests})
+
+        {:noreply,
+         socket
+         |> assign(
+           display_form: false,
+           changeset: Services.change_clients_request(%ClientsRequest{})
+         )
+         |> put_flash(:info, "Requête envoyée")
+         |> push_event("AnimateAlert", %{})}
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        {:noreply, socket |> assign(changeset: changeset)}
+    end
   end
 
 end
